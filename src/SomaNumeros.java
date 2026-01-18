9d4557903d1c1c1f6b09727a077ebece9012a539
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-01-18 13:55:36 -0300

    Usando funcoes para calculo

---
+++ src/SomaNumeros.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SomaNumeros {
+
+    public static int soma(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int v1, v2, s;
+
+        System.out.println("Digite o primeiro numero: ");
+        v1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        v2 = scanner.nextInt();
+
+        s = soma(v1, v2);
+
+        System.out.println("A soma entre " + v1 + " e " + v2 + " e igual a " + s);
+
+        scanner.close();
+    }
+}
