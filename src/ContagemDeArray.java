47d66a70826fd40de43e66521202dbe70addaa28
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-01-21 20:22:13 -0300

    exercicio básico de Vetor(Array)

---
+++ src/ContagemDeArray.java
@@ -0,0 +1,11 @@
+public class ContagemDeArray {
+        static int [] numeros = new int[10];
+
+
+    public static void main(String[] args) {
+        numeros[0] = 10;
+        numeros[1] = 20;
+        System.out.println(numeros [1]);
+    }
+
+}
