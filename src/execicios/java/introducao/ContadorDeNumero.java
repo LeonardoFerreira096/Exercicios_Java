6acfa1be6074a6e15ba7ba26b7b9a1429984d9fc
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-01-28 22:59:28 -0300

    exercicio usando laço FOR

---
+++ src/execicios/java/introducao/ContadorDeNumero.java
@@ -0,0 +1,14 @@
+package execicios.java.introducao;
+
+// pegando só numeros pares
+
+public class ContadorDeNumero {
+    public static void main(String[] args){
+        int i ;
+
+        for (i = 0; i <= 10; i+=2){
+            System.out.println(i);
+        }
+
+    }
+}
