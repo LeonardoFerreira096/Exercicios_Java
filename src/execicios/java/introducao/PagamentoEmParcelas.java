77959abf765629dd46d56a3d4c652f4daf1ef624
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-01-28 23:28:29 -0300

    exercicio usando laço FOR com breack

---
+++ src/execicios/java/introducao/PagamentoEmParcelas.java
@@ -0,0 +1,16 @@
+package execicios.java.introducao;
+
+public class PagamentoEmParcelas {
+    public static void main(String[] args){
+        double ValorCarro = 20000;
+
+        for (int parcela = 1; parcela <= ValorCarro ; parcela++) {
+            double ValorParcela = ValorCarro / parcela;
+            if (ValorParcela < 1000) {
+                break;
+            }
+            System.out.println(" Parcela " + parcela + " R$ " + ValorParcela);
+        }
+    }
+
+}
