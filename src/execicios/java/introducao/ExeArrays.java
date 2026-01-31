74be18fd6947b135e831abed041383ea44b83c2e
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-01-30 23:27:45 -0300

    exercicios e estudo sobre arrays e arrays multi(matriz)

---
+++ src/execicios/java/introducao/ExeArrays.java
@@ -0,0 +1,16 @@
+package execicios.java.introducao;
+
+public class ExeArrays {
+    public static void main(String[] args){
+        String [] nomes = new String[3];
+
+        nomes[0] = "Maria";
+        nomes[1] = "Leonardo";
+        nomes[2] = "Inessa";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+    }
+}
