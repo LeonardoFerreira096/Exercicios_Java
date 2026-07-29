83bbc610436456125072504249e416a7de455597
Leonardo Ferreira <leonardo_costaferreira22@hotmail.com>
2026-07-29 15:35:48 -0300

    ëxercicios da atividade 02 - irede-p2

--- src/iredejava/atividades/atividade02/exercicio10/Teste.java
+++ src/iredejava/atividades/atividade02/exercicio10/Teste.java
@@ -4,6 +4,7 @@ public class Teste {
     public static void main(String[] args) {
         Pagamento pagamentoCartao = new PagamentoCartao();
         Pagamento pagamentoPix = new PagamentoPix();
+        
         pagamentoCartao.processarPagamento();
         pagamentoPix.processarPagamento();
     }
